ea8832aa80a916e315d3d77e2138db5aa9e92efd
prusswan <prusswan@gmail.com>
2016-07-12 12:49:34 +0800

    updated references to 2016 May

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module SBF2015Nov
+module SBF2016May
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_SBF2015Nov_session'
+Rails.application.config.session_store :cookie_store, key: '_SBF2016May_session'
