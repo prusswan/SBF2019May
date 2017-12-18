463be2903b5672c12a989a4de67b99ce8ab38f6e
prusswan <prusswan@gmail.com>
2017-12-18 22:55:56 +0800

    updated references to 2017 May

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_SBF2016Nov_session'
+Rails.application.config.session_store :cookie_store, key: '_SBF2017May_session'
