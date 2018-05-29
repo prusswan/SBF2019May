89648a541d97769ff428d7e3c851024082af43b7
prusswan <prusswan@gmail.com>
2018-05-30 02:04:47 +0800

    serving blocks data as geojson

--- Gemfile
+++ Gemfile
@@ -43,6 +43,8 @@ gem 'settingslogic'
 gem 'activesupport-decorators', '~> 1.0'
 gem 'render_csv'
 
+gem 'rgeo-geojson'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/controllers/blocks_controller.rb
+++ app/controllers/blocks_controller.rb
@@ -9,6 +9,27 @@ class BlocksController < ApplicationController
           only: [:price, :area, :flat_type],
           add_methods: [:address, :long, :lat, :price_per_area, :delivery_date, :lease_start]
       end
+
+      format.json do
+        render json: Block.all,
+          only: [:id, :lat, :long, :address, :delivery_date, :lease_start],
+          methods: [:address]
+      end
+
+      format.geojson do
+        entity_factory = RGeo::GeoJSON::EntityFactory.instance
+        factory = RGeo::Geographic.simple_mercator_factory
+        result = Block.all.map do |b|
+          entity_factory.feature(factory.point(b.long, b.lat), b.id,
+            address: b.address,
+            delivery_date: b.delivery_date,
+            lease_start: b.lease_start,
+            link:  RailsAdmin::Engine.routes.url_helpers.show_path('block', b.id)
+          )
+        end
+
+        render json: RGeo::GeoJSON.encode(entity_factory.feature_collection(result))
+      end
     end
   end
 
