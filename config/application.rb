e4db127c9389d5b72256c57f68b4aabb7d2b3a22
William Ko <william_ko@sla.gov.sg>
2019-06-28 20:42:25 +0800

    updated references to 2019 May

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module SBF2018Nov
+module SBF2019May
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.0
